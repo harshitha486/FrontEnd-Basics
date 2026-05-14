7c98f02d9b02245ab926283b591dce6637e490ff
harshitha486 <harshithamedepally@gmail.com>
2026-05-14 17:54:25 +0530

    updated code

---
+++ 34.JS Loops/script.js
@@ -0,0 +1,31 @@
+
+// for (let i = 0; i < 100; i++) { 
+//     console.log(a + i); 
+// }
+
+// let obj = {
+//     name: "Harshitha",
+//     role: "Programmer",
+//     company: "MH"
+// }
+ 
+// for (const key in obj) { 
+//         let element = obj[key] ;
+//         console.log(key,element)
+// }
+
+// for (const c of "Harry") {
+//     console.log(c)
+// }
+
+// let i = 0;
+// while (i<60000) {
+//     console.log(i)
+//     i++;
+// }
+
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
---
+++ 36.JS Strings/script.js
@@ -0,0 +1,24 @@
+
+let a = "Harshitha";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[10]);
+
+console.log(a.length)
+
+let real_name = "Harshitha"
+let friend = "bunny"
+console.log("Her name is " + real_name + " and her friends name is " + friend)
+console.log(`Her name is ${real_name} and her friends name is ${friend}`)
+
+let b = "Harshitha"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase()) 
+console.log(b.length) 
+console.log(b.slice(1, 5)) 
+console.log(b.slice(1)) 
+
+console.log(b.replace("Sh", "77"))
+console.log(b.concat(a, "Harshi", "Harshi's"))
+
+console.log(b)
